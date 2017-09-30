26bab129db4352c668d6a20938c7382b52fe629a
kusano <kusano@users.noreply.github.com>
2017-10-01 05:50:28 +0900

    セットアップスクリプトと説明を追加

---
+++ README.md
@@ -0,0 +1,8 @@
+# ksnctf C92 problem
+
+ksnctf C92の問題です。
+
+`problems`ディレクトリを https://github.com/kusano/ksnctfc92 で指定することにより、コンテスト開催時と同じ問題が表示されます。
+
+`server/setup.sh` をCentOS 7 x86_64 minimalをインストールしたサーバーのrootが実行すると、ksnctfc92.u1tramarine.blueと同じ環境が整います。
+システム全体を書き換えるので、まっさらな環境以外では実行しないでください。
--- server/setup.sh
+++ server/setup.sh
@@ -1,7 +1,26 @@
+#!/bin/bash
 
+# システム全体を書き換えるので、まっさらな環境以外では実行しないでください
+# CentOS 7 x86_64 minimal向け
+
+set -e
+
+cat <<EOF > /etc/selinux/config
+SELINUX=disabled
+EOF
+setenforce 0
+
+yum -y update
+
+cd
+
+yum -y install git
+git clone --depth 1 https://github.com/kusano/ksnctfc92_problem.git
+
+# md5
 useradd md5
-md5の中身を/home/md5にコピー
 
+cp -r ksnctfc92_problem/server/md5 /home/
 chown -R root:md5 /home/md5
 chmod 750 /home/md5
 chmod 750 /home/md5/md5
@@ -9,58 +28,47 @@ chmod 750 /home/md5/md5.sh
 chmod 640 /home/md5/flag.txt
 chmod 640 /home/md5/flag2.txt
 
-yum install xinetd
-
-md5-streamを/etc/xinet.dにコピー
-chown root:root /etc/xinetd.d/md5-stream
+yum -y install xinetd
+cp ksnctfc92_problem/server/md5-stream /etc/xinetd.d/
 chmod 600 /etc/xinetd.d/md5-stream
 systemctl enable xinetd
-systemctl start xinetd
 
 firewall-cmd --add-port 55555/tcp --permanent
-firewall-cmd --add-port 55555/tcp
 
-yum install httpd
+yum -y install httpd
 systemctl enable httpd
-systemctl start httpd
 firewall-cmd --add-port 80/tcp --permanent
-firewall-cmd --add-port 80/tcp
 ln -s /lib64/libc.so.6 /var/www/html/libc.so.6
 
+# kaisendon
 useradd kaisendon
-../W/kaisendon.zipを/home/kaisendonにコピー
-yum install unzip
-unzip kaisendon.zip
-rm kaisendon.zip
-../W/config.jsを/home/kaisendon/kaisendonにコピー
+
+yum -y install unzip
+unzip ksnctfc92_problem/problems/W/kaisendon.zip -d /home/kaisendon/
+cp ksnctfc92_problem/W/config.js /home/kaisendon/kaisendon/
 chown -R kaisendon:kaisendon /home/kaisendon
 chmod -R 700 /home/kaisendon
 
-yum install epel-release
-yum install npm --enablerepo=epel
-
+yum -y install epel-release
+yum -y install npm --enablerepo=epel
 npm install -g forever
 
-firewall-cmd --add-port 3000/tcp --permanent
-firewall-cmd --add-port 3000/tcp
+cd /home/kaisendon/kaisendon
+sudo -u kaisendon npm install
+cd -
 
-su kaisendon
-cd ~/kaisendon
-npm install
-
-crontab -e
+cat <<EOF | sudo -u kaisendon crontab
 @reboot NODE_ENV=production FLAG1=FLAG{3snoa6p4wncj1hpf} FLAG2=FLAG{vakdriti4zzlj55p} FLAG3=FLAG{bpodf2es4k9e42rf} forever start /home/kaisendon/kaisendon/bin/www
+EOF
 
+firewall-cmd --add-port 3000/tcp --permanent
+
+# restrict
 chmod u-s /usr/bin/*
 chmod g-s /usr/bin/*
 chmod u+s /usr/bin/sudo
 chmod u+s /usr/bin/su
 chmod u+s /usr/bin/crontab
-
-
--A 
--A OUTPUT -m owner --uid-owner md5 -j REJECT
--A OUTPUT -m owner --uid-owner kaisendon -j REJECT
 
 firewall-cmd --direct --permanent --add-rule ipv4 filter OUTPUT 0 -m state --state RELATED,ESTABLISHED -j ACCEPT
 firewall-cmd --direct --permanent --add-rule ipv4 filter OUTPUT 999 -m owner --uid-owner kaisendon -j REJECT
@@ -68,4 +76,7 @@ firewall-cmd --direct --permanent --add-rule ipv4 filter OUTPUT 999 -m owner --u
 firewall-cmd --direct --permanent --add-rule ipv6 filter OUTPUT 0 -m state --state RELATED,ESTABLISHED -j ACCEPT
 firewall-cmd --direct --permanent --add-rule ipv6 filter OUTPUT 999 -m owner --uid-owner kaisendon -j REJECT
 firewall-cmd --direct --permanent --add-rule ipv6 filter OUTPUT 999 -m owner --uid-owner md5 -j REJECT
-firewall-cmd --reload
+
+# reboot
+rm -rf ksnctfc92_problem
+reboot
